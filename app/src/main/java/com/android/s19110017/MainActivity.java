7316612a952daba1a743d5ea242321c41b96b383
SatThu <minhduc.nt1602@gmail.com>
2022-04-01 14:40:31 +0700

    Hoàn thiện BTVN

--- app/src/main/java/com/android/s19110017/MainActivity.java
+++ app/src/main/java/com/android/s19110017/MainActivity.java
@@ -1,53 +1,115 @@
 package com.android.s19110017;
 
-import static java.lang.Double.parseDouble;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    //Khai báo biến
     EditText e1, e2, e3;
-    double num1, num2, num3;
+    Button btnClick;
+
+    //Lấy dữ liệu lại từ bên Result
+    ActivityResultLauncher<Intent> activityLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == RESULT_OK) {
+                        Intent intent = result.getData();
+                        if (intent != null) {
+                            int tienGui = intent.getIntExtra("tienGui", 0);
+                            int laiSuat = intent.getIntExtra("laiVaGoc", 0);
+                            int kyHan = intent.getIntExtra("kyHan", 0);
+                            e1.setText(tienGui + "");
+                            e2.setText(laiSuat + "");
+                            e3.setText(kyHan + "");
+                        }
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addControl();
+        addEvent();
     }
 
-    public boolean getNumbers() {
-        e1 = (EditText) findViewById(R.id.tiengui);
-        e2 = (EditText) findViewById(R.id.laisuat);
-        e3 = (EditText) findViewById(R.id.kyhan);
-
-        String s1 = e1.getText().toString();
-        String s2 = e2.getText().toString();
-        String s3 = e3.getText().toString();
-
-        if (s1 == " " || s1.equals(null) || s2 == " " || s2.equals(null) || s3 == " " || s3.equals(null)) {
-            return false;
-        } else {
-            num1 = Double.parseDouble(s1);
-            num2 = Double.parseDouble(s2);
-            num3 = Double.parseDouble(s3);
-            return true;
-        }
+    void addControl() {
+        e1 = findViewById(R.id.tiengui);
+        e2 = findViewById(R.id.laisuat);
+        e3 = findViewById(R.id.kyhan);
+        btnClick = findViewById(R.id.btn_result);
     }
 
-    public void TinhLaiSuat(View v) {
-        double sum = 0;
-        if (getNumbers()) {
-            sum = num1 * num2 * num3 / 360;
-        }
-        String s1 = e1.getText().toString();
-        double full = sum + parseDouble(s1);
-        Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-        intent.putExtra("tonglai", sum);
-        intent.putExtra("sotiengui", full);
-        startActivity(intent);
+    void addEvent() {
+        e1.addTextChangedListener(onTextChangedListener());
+        btnClick.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ResultActivity.class);
+                int tienGui = Integer.parseInt(e1.getText().toString().replaceAll(",", ""));
+                int laiSuat = Integer.parseInt(e2.getText().toString());
+                int kyHan = Integer.parseInt(e3.getText().toString());
+                int result = (tienGui * laiSuat * kyHan / 12) / 100;
+                Bundle bundle = new Bundle();
+                bundle.putInt("tienGui", tienGui);
+                bundle.putInt("laiSuat", laiSuat);
+                bundle.putInt("kyHan", kyHan);
+                bundle.putInt("tienlai", result);
+                bundle.putInt("tongtiennhan", result + tienGui);
+                intent.putExtra("dulieubundle", bundle);
+                activityLauncher.launch(intent);
+            }
+        });
+    }
+
+    private TextWatcher onTextChangedListener() {
+        return new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                e1.removeTextChangedListener(this);
+                try {
+                    String originalString = s.toString();
+                    Long longval;
+                    if (originalString.contains(",")) {
+                        originalString = originalString.replaceAll(",", "");
+                    }
+                    longval = Long.parseLong(originalString);
+                    DecimalFormat formatter = (DecimalFormat) NumberFormat.getInstance(Locale.US);
+                    formatter.applyPattern("#,###,###,###");
+                    String formattedString = formatter.format(longval);
+                    //Cấu hình text sau khi format thành EditText
+                    e1.setText(formattedString);
+                    e1.setSelection(e1.getText().length());
+                } catch (NumberFormatException nfe) {
+                    nfe.printStackTrace();
+                }
+                e1.addTextChangedListener(this);
+            }
+        };
     }
 }
--- app/src/main/java/com/android/s19110017/ResultActivity.java
+++ app/src/main/java/com/android/s19110017/ResultActivity.java
@@ -1,41 +1,98 @@
 package com.android.s19110017;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
-import java.io.IOException;
+import java.text.DecimalFormat;
 
 public class ResultActivity extends AppCompatActivity {
+    //Khai báo biến
     TextView t1, t2;
+    Button btnTake, btnBack;
+    ImageView imgView;
+
+    //Lấy dữ liệu lại từ bên Result
+    ActivityResultLauncher<Intent> activityCamera = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    Intent intent = result.getData();
+                    if (intent != null) {
+                        Bitmap bitmap = (Bitmap) intent.getExtras().get("data");//"Data" là mặc định
+                        imgView.setImageBitmap(bitmap);
+                        Intent intentX = new Intent(ResultActivity.this, MainActivity.class);
+                        startActivity(intentX);
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
-        t1 = (TextView) findViewById(R.id.tienlai);
-
-        t2 = (TextView) findViewById(R.id.tongtien);
-        Intent intent = getIntent();
-        double a = intent.getDoubleExtra("tonglai", 0.0);
-        String lai = Double.toString(a);
-        double b = intent.getDoubleExtra("sotiengui", 0.0);
-        String tongtien = Double.toString(b);
-        t1.setText(lai);
-        t2.setText(tongtien);
+        addControl();
+        addEvent();
+        processData();
     }
 
-    public void Camera(View view) throws IOException {
-        Intent intent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-        int REQUEST_ID_IMAGE_CAPTURE = 100;
-        // Start camera and wait for the results.
-        this.startActivityForResult(intent, REQUEST_ID_IMAGE_CAPTURE);
-        if (intent.resolveActivity(getPackageManager()) == null) {
-            Intent intent2 = new Intent(ResultActivity.this, MainActivity.class);
-            startActivity(intent2);
+    void addControl() {
+        t1 = (TextView) findViewById(R.id.tienlai);
+        t2 = (TextView) findViewById(R.id.tongtien);
+        btnTake = (Button) findViewById(R.id.btn_result);
+        btnBack = (Button) findViewById(R.id.btn_back);
+        imgView = (ImageView) findViewById(R.id.imageView);
+    }
+
+    void addEvent() {
+        btnTake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                activityCamera.launch(intent);
+            }
+        });
+
+        //Đưa dữ liệu lại MainActivity
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = getIntent();
+                Bundle bundle = intent.getBundleExtra("dulieubundle");
+                int tienGui = bundle.getInt("tienGui", 0);
+                int laiSuat = bundle.getInt("laiSuat", 0);
+                int kyHan = bundle.getInt("kyHan", 0);
+
+                Intent intentback = new Intent();
+                intentback.putExtra("tienGui", tienGui);
+                intentback.putExtra("laiVaGoc", laiSuat);
+                intentback.putExtra("kyHan", kyHan);
+                setResult(RESULT_OK, intentback);
+                finish();
+            }
+        });
+    }
+
+    void processData() {
+        Intent intent = getIntent();
+        Bundle bundle = intent.getBundleExtra("dulieubundle");
+        if (bundle != null) {
+            DecimalFormat formatnumber = new DecimalFormat("#,###,###,###");
+            int laiThucTe = bundle.getInt("tienlai", 0);
+            int laiVaGoc = bundle.getInt("tongtiennhan", 0);
+            t1.setText(formatnumber.format(laiThucTe) + " đ");
+            t2.setText(formatnumber.format(laiVaGoc) + " đ");
         }
     }
 }
